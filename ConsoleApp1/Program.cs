9732da551b7718106a3a818e9c47328b6bb4fdf8
Derstine Buyagan <derstinebuyagan@gmail.com>
2022-06-11 02:31:51 +0800

    Save

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+namespace ConsoleApp1
+{
+    using System;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+}
